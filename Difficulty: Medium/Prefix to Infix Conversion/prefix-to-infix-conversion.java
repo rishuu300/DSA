b454bd1fa811869881b569f9c7c1ce831e2d005b
Rishu Bhardwaj <rishuu300@gmail.com>
2024-12-09 10:43:58 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Prefix to Infix Conversion/prefix-to-infix-conversion.java
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.math.*;
+import java.io.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T-- > 0) {
+            sc.nextLine();
+            String s = sc.next();
+            Solution obj = new Solution();
+            String ans = obj.preToInfix(s);
+            System.out.println(ans);
+        
+System.out.println("~");
+}
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    static boolean isOperand(char ch){
+        return ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z') || (ch >= '0' && ch <= '9'));
+    }
+    
+    static String preToInfix(String pre_exp) {
+        Stack<String> s = new Stack<>();
+        
+        for(int i=pre_exp.length()-1; i>=0; i--){
+            char ch = pre_exp.charAt(i);
+            
+            if(isOperand(ch)){
+                s.push(ch + "");
+            }
+            else{
+                String op1 = s.pop();
+                String op2 = s.pop();
+                s.push("(" + op1 + ch + op2 + ")");
+            }
+        }
+        
+        return s.peek();
+    }
+}
