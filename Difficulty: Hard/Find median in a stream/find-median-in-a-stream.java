76ec798d3ca852846dd9f8eb031f0008e6205bf5
Rishu Bhardwaj <rishuu300@gmail.com>
2025-02-20 12:03:39 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Find median in a stream/find-median-in-a-stream.java
@@ -0,0 +1,68 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        sc.nextLine(); // Consume the newline character
+
+        while (t-- > 0) {
+            String s = sc.nextLine();
+            String[] parts = s.split(" ");
+            int[] nums = new int[parts.length];
+            for (int i = 0; i < parts.length; i++) {
+                nums[i] = Integer.parseInt(parts[i]);
+            }
+            Solution ob = new Solution();
+            ArrayList<Double> ans = ob.getMedian(nums);
+            for (double i : ans) {
+                System.out.printf("%.1f ", i);
+            }
+            System.out.println();
+            System.out.println("~");
+        }
+        sc.close();
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    public ArrayList<Double> getMedian(int[] arr) {
+        ArrayList<Double> result = new ArrayList<>();
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        
+        for (int num : arr) {
+            addNumber(num, maxHeap, minHeap);
+            result.add(findMedian(maxHeap, minHeap));
+        }
+        
+        return result;
+    }
+    
+    private void addNumber(int num, PriorityQueue<Integer> maxHeap, PriorityQueue<Integer> minHeap) {
+        if (maxHeap.isEmpty() || num <= maxHeap.peek()) {
+            maxHeap.offer(num);
+        } else {
+            minHeap.offer(num);
+        }
+        
+        if (maxHeap.size() > minHeap.size() + 1) {
+            minHeap.offer(maxHeap.poll());
+        } else if (minHeap.size() > maxHeap.size()) {
+            maxHeap.offer(minHeap.poll());
+        }
+    }
+
+    private double findMedian(PriorityQueue<Integer> maxHeap, PriorityQueue<Integer> minHeap) {
+        if (maxHeap.size() > minHeap.size()) {
+            return (double) maxHeap.peek();
+        } else {
+            return (maxHeap.peek() + minHeap.peek()) / 2.0;
+        }
+    }
+}
