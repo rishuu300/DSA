5066f7efb71cf4047e3f86cbcde34640a7e40ee0
Rishu Bhardwaj <rishuu300@gmail.com>
2025-04-07 00:35:55 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Directed Graph Cycle/directed-graph-cycle.java
@@ -0,0 +1,63 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int tc = sc.nextInt();
+        while (tc-- > 0) {
+            int V = sc.nextInt();
+            int E = sc.nextInt();
+            int[][] edges = new int[E][2];
+            for (int i = 0; i < E; i++) {
+                edges[i][0] = sc.nextInt();
+                edges[i][1] = sc.nextInt();
+            }
+
+            Solution obj = new Solution();
+            boolean ans = obj.isCyclic(V, edges);
+            System.out.println(ans ? "true" : "false");
+        }
+        sc.close();
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+    public boolean isCyclic(int V, int[][] edges) {
+        int[] indegree = new int[V];
+        int count = 0;
+        
+        for (int[] edge : edges) {
+            int u = edge[0];
+            int v = edge[1];
+            indegree[v]++;
+        }
+        
+        Queue<Integer> q = new LinkedList<>();
+        for (int i = 0; i < V; i++) {
+            if (indegree[i] == 0) {
+                q.add(i);
+            }
+        }
+        
+        while (!q.isEmpty()) {
+            int curr = q.poll();
+            count++;
+            
+            for (int[] edge : edges) {
+                if (edge[0] == curr) {
+                    int neighbor = edge[1];
+                    indegree[neighbor]--;
+                    if (indegree[neighbor] == 0) {
+                        q.add(neighbor);
+                    }
+                }
+            }
+        }
+        
+        return count != V;
+    }
+}
