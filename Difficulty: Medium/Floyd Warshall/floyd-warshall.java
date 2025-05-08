136d6cb0f46933ca9519a317cf7e3ddc2e976d37
Rishu Bhardwaj <rishuu300@gmail.com>
2025-05-08 11:08:22 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Floyd Warshall/floyd-warshall.java
+++ Difficulty: Medium/Floyd Warshall/floyd-warshall.java
@@ -39,12 +39,11 @@ class Solution {
     public void floydWarshall(int[][] dist) {
         int n = dist.length;
         
-        for (int k = 0; k < n; k++) {
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < n; j++) {
-                    if ((long)dist[i][k] + (long)dist[k][j] < dist[i][j]) {
-                        dist[i][j] = dist[i][k] + dist[k][j];
-                    }
+        for(int k=0; k<n; k++){
+            for(int i=0; i<n; i++){
+                for(int j=0; j<n; j++){
+                    if (dist[i][k] == (int)(1e8) || dist[k][j] == (int)(1e8)) continue;
+                    dist[i][j] = Math.min(dist[i][j], dist[i][k] + dist[k][j]);
                 }
             }
         }
