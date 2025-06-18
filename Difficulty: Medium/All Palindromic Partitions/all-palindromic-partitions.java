0e23518ea00bb148592e7f7a78c9104baebf17c5
Rishu Bhardwaj <rishuu300@gmail.com>
2025-06-18 13:23:36 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/All Palindromic Partitions/all-palindromic-partitions.java
@@ -0,0 +1,30 @@
+class Solution {
+    public ArrayList<ArrayList<String>> palinParts(String s) {
+        ArrayList<ArrayList<String>> result = new ArrayList<>();
+        backtrack(s, 0, new ArrayList<>(), result);
+        return result;
+    }
+
+    private void backtrack(String s, int start, ArrayList<String> current, ArrayList<ArrayList<String>> result) {
+        if (start == s.length()) {
+            result.add(new ArrayList<>(current));
+            return;
+        }
+        
+        for (int end = start; end < s.length(); end++) {
+            if (isPalindrome(s, start, end)) {
+                current.add(s.substring(start, end + 1));
+                backtrack(s, end + 1, current, result);
+                current.remove(current.size() - 1);
+            }
+        }
+    }
+
+    private boolean isPalindrome(String s, int left, int right) {
+        while (left < right) {
+            if (s.charAt(left++) != s.charAt(right--))
+                return false;
+        }
+        return true;
+    }
+}
