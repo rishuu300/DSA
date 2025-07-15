c2f38b233c09a3a0193b52047163c0a95cb41a9a
Rishu Bhardwaj <rishuu300@gmail.com>
2025-07-15 10:44:56 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Divisible by 13/divisible-by-13.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean divby13(String s) {
+        int rem = 0;
+        
+        for (int i = 0; i < s.length(); i++) {
+            rem = (rem * 10 + (s.charAt(i) - '0')) % 13;
+        }
+        
+        return rem == 0;
+    }
+}
