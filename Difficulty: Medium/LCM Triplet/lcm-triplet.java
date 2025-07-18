1bce9b538b4a33ccd3cc3777029bc547c6fb77ad
Rishu Bhardwaj <rishuu300@gmail.com>
2025-07-18 08:55:34 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/LCM Triplet/lcm-triplet.java
@@ -0,0 +1,23 @@
+class Solution {
+    int lcmTriplets(int n) {
+        int ans;
+
+        if (n < 3) {
+            ans = n;
+        }
+        
+        else if (n % 2 != 0) {
+            ans = n * (n - 1) * (n - 2);
+        }
+        
+        else if (n % 3 != 0) {
+            ans = n * (n - 1) * (n - 3);
+        }
+        
+        else {
+            ans = (n - 1) * (n - 2) * (n - 3);
+        }
+
+        return ans;
+    }
+}
