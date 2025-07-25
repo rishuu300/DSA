7a20deecbee7f19e33371eac0ee6429dc87df130
Rishu Bhardwaj <rishuu300@gmail.com>
2025-07-25 15:17:28 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Max Circular Subarray Sum/max-circular-subarray-sum.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int maxCircularSum(int arr[]) {
+        int n = arr.length;
+
+        int maxKadane = kadane(arr);
+
+        int totalSum = 0;
+        for (int i = 0; i < n; i++) {
+            totalSum += arr[i];
+            arr[i] = -arr[i];
+        }
+
+        int maxInvertedKadane = kadane(arr); 
+        int maxCircular = totalSum + maxInvertedKadane;
+
+        if (maxCircular == 0) {
+            return maxKadane;
+        }
+
+        return Math.max(maxKadane, maxCircular);
+    }
+    
+    private int kadane(int[] arr) {
+        int maxEndingHere = arr[0], maxSoFar = arr[0];
+        
+        for (int i = 1; i < arr.length; i++) {
+            maxEndingHere = Math.max(arr[i], maxEndingHere + arr[i]);
+            maxSoFar = Math.max(maxSoFar, maxEndingHere);
+        }
+        
+        return maxSoFar;
+    }
+}
