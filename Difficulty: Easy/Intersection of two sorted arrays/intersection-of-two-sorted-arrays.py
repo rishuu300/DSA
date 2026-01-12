38860f1f4c60e7389f420d2cb465f4ff9c42108c
Rishu Bhardwaj <rishuu300@gmail.com>
2026-01-12 14:05:13 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Intersection of two sorted arrays/intersection-of-two-sorted-arrays.py
+++ Difficulty: Easy/Intersection of two sorted arrays/intersection-of-two-sorted-arrays.py
@@ -1,11 +1,10 @@
 class Solution:
     def intersection(self, arr1, arr2):
         s = set()
+        ans = []
         
         for element in arr1:
             s.add(element)
-        
-        ans = []
         
         for element in arr2:
             if element in s:
