0ce0c4595a79c13d0f645d0d646164d8b6ea4e86
Rishu Bhardwaj <rishuu300@gmail.com>
2026-03-28 10:50:38 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Middle of a Linked List/middle-of-a-linked-list.py
@@ -0,0 +1,17 @@
+'''
+class node:
+    def __init__(data):
+        self.data = data
+        self.next = None
+'''
+
+class Solution:
+    def getMiddle(self, head):
+        slow = head
+        fast = head
+        
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        
+        return slow.data
